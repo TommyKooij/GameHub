a84fb97446f159004c5292d8fbbb37f833b8acc9
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-01-28 11:52:09 +0100

    28/01/2024
    - Fetch the games

--- Online_Video_Game_Shop/game-hub/src/components/GameGrid.tsx
+++ Online_Video_Game_Shop/game-hub/src/components/GameGrid.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Text } from "@chakra-ui/react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesRepsonse {
+  count: number;
+  results: Game[];
+}
 
 const GameGrid = () => {
-  return <div>GameGrid</div>;
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState([""]);
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesRepsonse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  });
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default GameGrid;
