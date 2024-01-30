9c38587ab3759060a1314ffbf1474ca4754d0e02
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-01-30 12:12:51 +0100

    30/01/2024 6
    - Filter games by genre

--- Online_Video_Game_Shop/game-hub/src/components/GenreList.tsx
+++ Online_Video_Game_Shop/game-hub/src/components/GenreList.tsx
@@ -1,10 +1,21 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageURL from "../services/image-url";
 
-const GenreList = () => {
-  const { data, isLoading, error } = useGenres();
-  if (error) return null;
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
+  const { data, isLoading } = useGenres();
   if (isLoading) return <Spinner />;
 
   return (
@@ -17,7 +28,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageURL(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
--- Online_Video_Game_Shop/game-hub/src/hooks/useGames.ts
+++ Online_Video_Game_Shop/game-hub/src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -14,6 +15,9 @@ export interface Game {
   metacritic: number;
 }
 
-const useGame = () => useData<Game>("/games");
+const useGame = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGame;
