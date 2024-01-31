534f56d891cf14dc906897a7fb11d14184bec1a5
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-01-31 10:41:27 +0100

    31/01/2024 4
    - Sort the games

--- Online_Video_Game_Shop/game-hub/src/hooks/useGames.ts
+++ Online_Video_Game_Shop/game-hub/src/hooks/useGames.ts
@@ -23,6 +23,7 @@ const useGame = (gameQuery: GameQuery) =>
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
       },
     },
     [gameQuery]
